0a6bf7350c582ee1c3c3cbc559bbafcf4a512ebb
Vasyl <admin@gmail.com>
2018-03-27 19:01:18 +0300

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Сайт </title>
+	<script src="js/script.js" type="text/javascript"></script>
+	<link href="css/main.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+	<h1>Сайт</h1>
+    </body>
+    </body>
+</html>
